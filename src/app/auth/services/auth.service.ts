62403c00e0008cbc35cee8866db5246f65ea5149
Sirinemn <sirinemn@outlook.fr>
2025-04-02 17:09:54 +0200

    add authentication service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = "http://localhost:8001/api/auth"
+
+  constructor(private httpclient: HttpClient, private router: Router) { }
+
+  public login(loginRequest: LoginRequest): Observable<AuthResponse> {
+    return this.httpclient.post<AuthResponse>(`${this.apiUrl}/login`, loginRequest)
+  }
+}

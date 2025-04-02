f9ba8d28432b731a9b6f83eeb936adaf0e068ed2
Sirinemn <sirinemn@outlook.fr>
2025-04-02 17:31:57 +0200

    add methods

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginRequest } from '../models/loginRequest';
 import { AuthResponse } from '../models/auth-response';
+import { RegisterRequest } from '../models/registerRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,21 @@ export class AuthService {
   constructor(private httpclient: HttpClient, private router: Router) { }
 
   public login(loginRequest: LoginRequest): Observable<AuthResponse> {
-    return this.httpclient.post<AuthResponse>(`${this.apiUrl}/login`, loginRequest)
+    return this.httpclient.post<AuthResponse>(`${this.apiUrl}/login`, loginRequest);
+  }
+
+  public registerAdmin(registerRequest: RegisterRequest) : Observable<any> {
+    return this.httpclient.post(`${this.apiUrl}/register`, registerRequest);
+  }
+
+  public registerUser(registerRequest: RegisterRequest) : Observable<any> {
+    return this.httpclient.post(`${this.apiUrl}/register/user`, registerRequest);
+  }
+  public logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+  public isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
   }
 }

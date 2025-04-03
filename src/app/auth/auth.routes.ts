239ca92bb755b168cbe42e22027222bb7215a2a4
Sirinemn <sirinemn@outlook.fr>
2025-04-03 15:31:33 +0200

    add auth routes

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+
+export const auth_routes: Routes = [
+    {title: 'Login', path: 'login', component: LoginComponent},
+    {title: 'Register', path: 'register', component: RegisterComponent}
+] 

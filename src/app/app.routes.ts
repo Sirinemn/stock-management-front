0d09a79f136a7048fbdbb2fb0d221a7ea9cd93b6
Sirinemn <sirinemn@outlook.fr>
2025-04-03 16:35:22 +0200

    cleaning code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
-        path: '' , 
-        component: HomeComponent
+      path: '' , 
+      component: HomeComponent
     },
     {
-        path: 'auth', 
-        canActivate: [], 
-        loadChildren: ()=> 
-            import('./auth/auth.routes').then((route)=> route.auth_routes)
+      path: 'auth', 
+      canActivate: [], 
+      loadChildren: ()=> 
+        import('./auth/auth.routes').then((route)=> route.auth_routes)
     },
     { 
       path: '404',

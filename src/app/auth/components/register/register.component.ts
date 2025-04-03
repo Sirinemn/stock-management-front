7ad67334414c66690aec2cc2e419d233d464f6da
Sirinemn <sirinemn@outlook.fr>
2025-04-03 09:43:26 +0200

    implements login components

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { RegisterAdminRequest } from '../../models/registerRequest';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  public formGroup: FormGroup;
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+      this.formGroup = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],  
+        lastname: [  '', [Validators.required, Validators.min(4), Validators.max(20)], ],
+        firstname: [  '', [Validators.required, Validators.min(4), Validators.max(20)], ],
+        group: [  '', [Validators.required, Validators.min(4), Validators.max(20)], ],
+        dateOfBirth:  ['',[Validators.required],],
+        password: [ '', [Validators.required, Validators.min(4), Validators.max(40)],]
+      });
+  }
+  public Submit(): void {
+    const registerRequest = this.formGroup.value as RegisterAdminRequest
+    this.authService.registerAdmin(registerRequest).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response);
+        this.router.navigate(['/login']); 
+      },
+      error: (err) => {
+        console.error('Registration failed:', err);
+      }
+    });
+  }
+
+  public back() {
+    window.history.back();
+  }
 }

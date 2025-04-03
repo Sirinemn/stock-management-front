0ceec2042c7d792960abdaaf224f4004a991a129
Sirinemn <sirinemn@outlook.fr>
2025-04-03 12:42:53 +0200

    implements home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 

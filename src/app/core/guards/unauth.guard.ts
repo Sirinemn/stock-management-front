1634384020a328fd3eca51408a632abab85771f2
Sirinemn <sirinemn@outlook.fr>
2025-04-10 17:03:42 +0200

    add injectable decorator to guards

--- src/app/core/guards/unauth.guard.ts
+++ src/app/core/guards/unauth.guard.ts
@@ -1,6 +1,8 @@
 import { CanActivate, Router } from "@angular/router";
 import { SessionService } from "../services/session.service";
+import { Injectable } from "@angular/core";
 
+@Injectable({ providedIn: 'root' })
 export class UnauthGuard implements CanActivate {
   constructor(private sessionService: SessionService, private router: Router) {}
 

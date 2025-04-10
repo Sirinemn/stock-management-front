b0bf44c75d173d5360cea93621d0e6d00edaffea
Sirinemn <sirinemn@outlook.fr>
2025-04-10 20:43:27 +0200

    add jest.config file

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+module.exports = {
+    moduleNameMapper: {
+      '@core/(.*)': '<rootDir>/src/app/core/$1',
+    },
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+    bail: false,
+    verbose: false,
+    collectCoverage: true,
+    coverageDirectory: './coverage/jest',
+    coverageReporters: ['lcov', 'text'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+    coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
+    coverageThreshold: {
+      global: {
+        statements: 80
+      },
+    },
+    roots: [
+      "<rootDir>"
+    ],
+    modulePaths: [
+      "<rootDir>"
+    ],
+    moduleDirectories: [
+      "node_modules"
+    ],
+  };
+  

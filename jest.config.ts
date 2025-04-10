d698d43becc5cc7a7eb7b2bfe15a451039467446
Sirinemn <sirinemn@outlook.fr>
2025-04-10 20:49:32 +0200

    add setup.jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ module.exports = {
       '@core/(.*)': '<rootDir>/src/app/core/$1',
     },
     preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+    setupFilesAfterEnv: ['<rootDir>/setup.jest.ts'],
     bail: false,
     verbose: false,
     collectCoverage: true,

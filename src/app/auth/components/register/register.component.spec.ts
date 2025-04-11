187b54e6de1698183549719a4bde6b3bc9c52a19
Sirinemn <sirinemn@outlook.fr>
2025-04-11 14:34:19 +0200

    fix register test

--- src/app/auth/components/register/register.component.spec.ts
+++ src/app/auth/components/register/register.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { RegisterComponent } from './register.component';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let mockAuthService: any;
+  let mockRouter: any = { events: { subscribe: jest.fn() }, createUrlTree: jest.fn(), serializeUrl: jest.fn() };
+  let mockSnackBar: any;
+
 
   beforeEach(async () => {
+    mockAuthService = { registerAdmin: jest.fn().mockReturnValue({ subscribe: jest.fn() }) };
+    const mockActivatedRoute = {
+      snapshot: { paramMap: { get: jest.fn().mockReturnValue('mockValue') } }
+    };
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      imports: [RegisterComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideNativeDateAdapter(),
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+      ]
     })
     .compileComponents();
 

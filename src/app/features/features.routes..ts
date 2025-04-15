68b3c02102bc75aa73d492a1a4b024960af7b32e
Sirinemn <sirinemn@outlook.fr>
2025-04-15 22:29:01 +0200

    add routes

--- src/app/features/features.routes..ts
+++ src/app/features/features.routes..ts
@@ -1,6 +1,10 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { RegisterUserComponent } from "./users/components/register-user/register-user.component";
+import { UserFormComponent } from "./users/components/user-form/user-form.component";
 
 export const features_routes: Routes = [
     {title: 'Dashboard', path: 'dashboard', component: DashboardComponent},
+    {title: 'AddUser', path: 'adduser', component: RegisterUserComponent},
+    {title: 'EditUser', path: 'edituser/:id', component: UserFormComponent},
 ]

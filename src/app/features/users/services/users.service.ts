40caa27cb989a60eb5cc66e7cfaba268cfb9c2ae
Sirinemn <sirinemn@outlook.fr>
2025-04-15 19:58:10 +0200

    add users service

---
+++ src/app/features/users/services/users.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../../auth/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private apiUrl = "http://localhost:8001/api/admin"
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.apiUrl}/users`);
+  }
+  public getUser(id: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+  public deleteUser(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/users/${id}`);
+  }
+}

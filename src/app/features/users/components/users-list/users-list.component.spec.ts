8f9ec251ac6e793f937a7bfa2555408ba02e1431
Sirinemn <sirinemn@outlook.fr>
2025-04-15 20:24:37 +0200

    implement user list

--- src/app/features/users/components/users-list/users-list.component.spec.ts
+++ src/app/features/users/components/users-list/users-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersListComponent } from './users-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('UsersListComponent', () => {
   let component: UsersListComponent;
@@ -8,7 +10,11 @@ describe('UsersListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UsersListComponent]
+      imports: [UsersListComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting() 
+      ]
     })
     .compileComponents();
 
--- src/app/features/users/components/users-list/users-list.component.ts
+++ src/app/features/users/components/users-list/users-list.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../../../auth/models/user';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [MatIconModule, MatTableModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit, OnDestroy {
+
+  public users: User[] = [];
+  displayedColumns: string[] = ['firstname', 'lastname', 'email'];
+  public loading: boolean = false;
+  public httpSubscription: Subscription = new Subscription();
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  public getUsers(): void {
+    this.loading = true;
+    this.userService.getUsers().subscribe({
+      next: (users: User[]) => {
+        this.users = users;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+  public back() {
+    window.history.back();
+  }
+  deleteUser(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  editUser(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  viewUser(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    this.httpSubscription.unsubscribe();
+  }
 
 }

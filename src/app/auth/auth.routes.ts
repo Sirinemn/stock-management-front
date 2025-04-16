57e29150f84d5ddf181686d0ad041b18727e0ef9
Sirinemn <sirinemn@outlook.fr>
2025-04-16 19:27:56 +0200

    add change-password route

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { ChangePasswordComponent } from "./components/change-password/change-password.component";
 
 export const auth_routes: Routes = [
     {title: 'Login', path: 'login', component: LoginComponent},
-    {title: 'Register', path: 'register', component: RegisterComponent}
+    {title: 'Register', path: 'register', component: RegisterComponent},
+    {title: 'Change Password', path: 'change-password', component: ChangePasswordComponent}, 
 ] 

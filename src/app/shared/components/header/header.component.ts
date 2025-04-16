a003c960b25eafd431fdb715fbfc4c235d3a044d
Sirinemn <sirinemn@outlook.fr>
2025-04-16 22:39:06 +0200

    add  isAdmin condition to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../../../auth/services/auth.service';
 import { SessionService } from '../../../core/services/session.service';
 import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-imports: [MatToolbarModule, RouterLink],
+imports: [MatToolbarModule, RouterLink, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public isAmin: boolean = false;
   constructor(
     private authService: AuthService,
     private sessionService: SessionService,
   ) { }
+
+  ngOnInit(): void {
+    this.isAmin = this.sessionService.getUser()?.roles.includes('ADMIN') || false;
+  }
+
   public logOut() {
     this.authService.logout();
     this.sessionService.logOut();

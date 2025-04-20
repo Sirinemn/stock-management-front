cff5e827ef3bc2b5f2e85a78f381fe9d98b2c40b
Sirinemn <sirinemn@outlook.fr>
2025-04-20 17:20:42 +0200

    implément auth state service

--- src/app/core/services/auth-state.service.ts
+++ src/app/core/services/auth-state.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthStateService {
+  public userRole!:string[];
+  private firstLoginSubject = new BehaviorSubject<boolean>(false);
+  private isAdminSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+    public setFirstLogin(value: boolean) {
+      this.firstLoginSubject.next(value);
+    }
+    public setIsAdmin(value: boolean) {
+      this.isAdminSubject.next(value);
+    }
+    public getIsAdmin(): Observable<boolean> {
+      return this.isAdminSubject.asObservable();
+    }
+    public getFirstLogin(): Observable<boolean> {
+      return this.firstLoginSubject.asObservable();
+    }
+    public isAdmin(): boolean {
+      return this.userRole.includes('ADMIN');
+    }
 }

4bef17630b48734b474c0759702b0cb76b5e2e1a
Sirinemn <sirinemn@outlook.fr>
2025-04-22 10:52:06 +0200

    implement user-detail

--- src/app/features/admin/components/user-detail/user-detail.component.ts
+++ src/app/features/admin/components/user-detail/user-detail.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { User } from '../../../../auth/models/user';
+import { UsersService } from '../../services/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [MatIconModule, MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit , OnDestroy {
+  public loading: boolean = false;
+  public user!: User;
+  public userId: number = 0;
+
+  constructor(
+    private userService: UsersService,
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.userId = this.router.parseUrl(this.router.url).queryParams['id'] || 0;
+  }
+
+  public back() {
+    window.history.back();
+  }
+  public getUser(): void {
+    this.loading = true;
+    this.userService.getUser(this.userId).subscribe({
+      next: (user: User) => {
+        this.user = user;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    // Cleanup logic if needed
+  }
 
 }

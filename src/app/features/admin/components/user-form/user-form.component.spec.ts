2f37f21da36deb6c17708c9aba29e736fdd48e74
Sirinemn <sirinemn@outlook.fr>
2025-04-22 16:31:53 +0200

    migrate register logic to user form component

--- src/app/features/admin/components/user-form/user-form.component.spec.ts
+++ src/app/features/admin/components/user-form/user-form.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserFormComponent } from './user-form.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
   let fixture: ComponentFixture<UserFormComponent>;
+  let mockRouter: any = { events: { subscribe: jest.fn() }, createUrlTree: jest.fn(), serializeUrl: jest.fn() };
+  const mockActivatedRoute = {
+    snapshot: { paramMap: { get: jest.fn().mockReturnValue('mockValue') } }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserFormComponent]
+      imports: [UserFormComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ]
     })
     .compileComponents();
 
--- src/app/features/admin/components/user-form/user-form.component.ts
+++ src/app/features/admin/components/user-form/user-form.component.ts
@@ -1,11 +1,139 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../../../../auth/services/auth.service';
+import { RegisterUserRequest } from '../../../../auth/models/registerUserRequest';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../../../auth/models/user';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatProgressSpinnerModule ,MatCardModule, MatDatepickerModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, NgIf],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
+  public formGroup: FormGroup;
+  public errorMessage: string = '';
+  public messageResponse: string = '';
+  private destroy$ = new Subject<void>();
+  isLoading: boolean = false;
+  public isEditMode: boolean = false; 
+  public userId: string | null = null; 
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UsersService,
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {
+    this.formGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],  
+      lastname: [  '', [Validators.required, Validators.min(4), Validators.max(20)], ],
+      firstname: [  '', [Validators.required, Validators.min(4), Validators.max(20)], ],
+      dateOfBirth:  ['',[Validators.required],],
+      password: ['', this.getPasswordValidators()]
+    });
+  }
+  ngOnInit(): void {
+      this.userId = this.route.snapshot.paramMap.get('id') || null;
+      if (this.userId) {
+        this.isEditMode = true; 
+        this.updatePasswordValidators();
+        this.authService.getUser(+this.userId).pipe(takeUntil(this.destroy$)).subscribe({
+          next: (user) => {
+            this.formGroup.patchValue({
+              email: user.email,
+              lastname: user.lastname,
+              firstname: user.firstname,
+              dateOfBirth: user.dateOfBirth,
+            });
+            console.log("Valeurs après patchValue :", this.formGroup.value);
+          },
+          error: (error) => {
+            this.errorMessage = error.error.message;
+          }
+        });
+      }
+  }
+  private getPasswordValidators(): ValidatorFn[] {
+    return this.isEditMode ? [] : [Validators.required, Validators.minLength(8)];
+  }
+  private updatePasswordValidators(): void {
+    const passwordControl = this.formGroup.get('password');
+    if (passwordControl) {
+      passwordControl.clearValidators();
+      if (!this.isEditMode) {
+        passwordControl.setValidators([Validators.required, Validators.minLength(8)]);
+      }
+      passwordControl.updateValueAndValidity();
+    }
+  }
+
+  public AddUser(): void {
+    if (this.formGroup.valid) {
+      this.isLoading = true; 
+      const registerUser = this.formGroup.value as RegisterUserRequest
+      this.authService.registerUser(registerUser).pipe(takeUntil(this.destroy$)).subscribe({
+        next: (response) => {
+          this.isLoading = false; 
+          this.messageResponse = response.message;
+          const snackBarRef = this.snackBar.open(this.messageResponse, 'Fermer', {
+            duration: 3000,
+          });
+          snackBarRef.afterDismissed().subscribe(() => {
+            this.formGroup.reset();
+            this.router.navigate(['/admin/users']);
+          });
+        },
+        error: (error) => {
+          this.isLoading = false; 
+          this.errorMessage = error.error.message;
+        }
+      });
+    }
+  }
+  public updateUser(): void {
+    if (this.formGroup.valid) {
+      this.isLoading = true; 
+      const registerUser = this.formGroup.value as User
+      this.userService.updateUser(+this.userId!, registerUser).pipe(takeUntil(this.destroy$)).subscribe({
+        next: (response) => {
+          this.isLoading = false; 
+          this.messageResponse = response.message;
+          const snackBarRef = this.snackBar.open(this.messageResponse, 'Fermer', {
+            duration: 3000,
+          });
+          snackBarRef.afterDismissed().subscribe(() => {
+            this.formGroup.reset();
+            this.router.navigate(['/admin/users']);
+          });
+        },
+        error: (error) => {
+          this.isLoading = false; 
+          this.errorMessage = error.error.message;
+        }
+      });
+    }
+  }
+  public back() {
+    window.history.back();
+  }
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }

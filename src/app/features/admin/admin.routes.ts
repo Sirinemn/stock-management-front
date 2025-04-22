657c10e45c70d50ef1107ae120a3b5c9dbdfa5d5
Sirinemn <sirinemn@outlook.fr>
2025-04-22 10:53:02 +0200

    add view and edit user route to adminroute

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { RegisterUserComponent } from "./components/register-user/register-user.component";
 import { UserFormComponent } from "./components/user-form/user-form.component";
 import { UsersListComponent } from "./components/users-list/users-list.component";
+import { UserDetailComponent } from "./components/user-detail/user-detail.component";
 
 export const admin : Routes = [
     {title: 'Users', path: 'users', component: UsersListComponent},
     {title: 'AddUser', path: 'adduser', component: RegisterUserComponent},
-    {title: 'EditUser', path: 'edituser/:id', component: UserFormComponent},
+    {title: 'UserDetail', path: 'user/view/:id', component: UserDetailComponent},
+    {title: 'EditUser', path: 'user/edit/:id', component: UserFormComponent},
 ]

0172fb4a1e14d18a63db2049cf109906d85f4eb9
Sirinemn <sirinemn@outlook.fr>
2025-04-22 17:06:49 +0200

    implement confirm dialog

--- src/app/mat-dialog/confirm-dialog/confirm-dialog.component.ts
+++ src/app/mat-dialog/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
-  imports: [],
+  imports: [
+    MatDialogModule, MatButtonModule
+  ],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.scss'
 })
 export class ConfirmDialogComponent {
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }

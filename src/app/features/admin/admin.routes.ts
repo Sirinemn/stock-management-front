8b11d4b142757ea21687b5f9e990dcaaf7e8f41e
Sirinemn <sirinemn@outlook.fr>
2025-04-24 15:46:11 +0200

    add categories route

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from "@angular/router";
 import { UserFormComponent } from "./users/user-form/user-form.component";
 import { UsersListComponent } from "./users/users-list/users-list.component";
 import { UserDetailComponent } from "./users/user-detail/user-detail.component";
+import { CategoryListComponent } from "./categories/category-list/category-list.component";
 
 export const admin : Routes = [
     {title: 'Users', path: 'users', component: UsersListComponent},
     {title: 'AddUser', path: 'adduser', component: UserFormComponent},
     {title: 'UserDetail', path: 'user/view/:id', component: UserDetailComponent},
     {title: 'EditUser', path: 'user/edit/:id', component: UserFormComponent},
+    {title: 'Categories', path: 'features/categories', component: CategoryListComponent}
 ]

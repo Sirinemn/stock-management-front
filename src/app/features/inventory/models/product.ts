d89d6ee80606d8b5dc9885e22b8f554cef360ed7
Sirinemn <sirinemn@outlook.fr>
2025-04-25 10:54:19 +0200

    add missing attribut

--- src/app/features/inventory/models/product.ts
+++ src/app/features/inventory/models/product.ts
@@ -6,6 +6,7 @@ export interface Product {
     price: number;
     threshold: number;
     userId: number;
+    userName: string;
     categoryId: number;
     catgoryName: string;
     groupId: number;

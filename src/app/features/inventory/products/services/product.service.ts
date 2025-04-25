e30ff0be3cbf6a2cb0180a78932c815595858773
Sirinemn <sirinemn@outlook.fr>
2025-04-25 11:06:05 +0200

    implement product service

--- src/app/features/inventory/products/services/product.service.ts
+++ src/app/features/inventory/products/services/product.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../../models/product';
+import { Observable } from 'rxjs';
+import { MessageResponse } from '../../../../shared/models/messageResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:8001/api/products"
+
+  constructor(private httpClient: HttpClient) { }
+
+  public addProduct(product: Product): Observable<MessageResponse> {
+    return this.httpClient.post<MessageResponse>(`${this.apiUrl}`, product);
+  }
+  public getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`${this.apiUrl}`);
+  }
+  public getProduct(id: number): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.apiUrl}/${id}`);
+  }
+  public updateProduct(id: number, product: Product): Observable<MessageResponse> {
+    return this.httpClient.put<MessageResponse>(`${this.apiUrl}/${id}`, product);
+  }
+  public deleteProduct(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }

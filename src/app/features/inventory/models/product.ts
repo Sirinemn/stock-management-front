c8b2dc6457e66744ed4a8a6f9e4163e257ed9dcd
Sirinemn <sirinemn@outlook.fr>
2025-04-25 10:43:23 +0200

    add product interface

---
+++ src/app/features/inventory/models/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    quantity: number;
+    price: number;
+    threshold: number;
+    userId: number;
+    categoryId: number;
+    catgoryName: string;
+    groupId: number;
+    groupName: string;
+}

61bde04bca2896e6507f375a1936c54d3153d762
Sirinemn <sirinemn@outlook.fr>
2025-04-25 09:59:05 +0200

    add category routes

---
+++ src/app/features/admin/categories/category.routes.ts
@@ -0,0 +1,5 @@
+import { CategoryListComponent } from "./category-list/category-list.component";
+
+export const categoryRoutes = [
+    {title: 'Categories', path: 'categories', component: CategoryListComponent}
+]

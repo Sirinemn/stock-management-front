06bef057556c8ad4d4dfce723523cd4afc850131
Sirinemn <sirinemn@outlook.fr>
2025-04-27 12:15:45 +0200

    update categorie list

--- src/app/features/admin/categories/category-list/category-list.component.spec.ts
+++ src/app/features/admin/categories/category-list/category-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoryListComponent } from './category-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CategoryListComponent', () => {
   let component: CategoryListComponent;
@@ -8,7 +10,11 @@ describe('CategoryListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CategoryListComponent]
+      imports: [CategoryListComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
     })
     .compileComponents();
 
--- src/app/features/admin/categories/category-list/category-list.component.ts
+++ src/app/features/admin/categories/category-list/category-list.component.ts
@@ -1,11 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { Category } from '../../models/category';
+import { Subject, takeUntil } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { SessionService } from '../../../../core/services/session.service';
+import { User } from '../../../../auth/models/user';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [MatIconModule, MatTableModule, MatButtonModule, NgIf, FormsModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.scss'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit, OnDestroy {
+  public categories: Category[] = [];
+  public newCategoryName: string = '';
+  public user: User | null = null;
+  private userId: number = 0;
+  public isAddingCategory: boolean = false;
+  public errorMessage: string = '';
+  public loading: boolean = false;
+  private destroy$ = new Subject<void>();
+  public displayedColumns: string[] = ['index', 'name', 'actions'];
 
+  constructor(
+    private adminService: AdminService,
+    private sessionService: SessionService,
+  ) { }
+  
+  ngOnInit(): void {
+    this.getCategories();
+    this.user = this.sessionService.getUser() ?? null;
+    if (this.user) {
+      this.userId = this.user.id;
+    }
+  }
+  public toggleAddCategory() {
+    this.isAddingCategory = !this.isAddingCategory;
+  }
+  public addCategory() {
+    if (this.newCategoryName.trim() === '') return;
+    this.loading = true;
+    this.adminService.addCategory(this.newCategoryName, this.userId).pipe(takeUntil(this.destroy$)).subscribe({
+      next: () => {
+        this.getCategories();
+        this.isAddingCategory = false;
+        this.newCategoryName = '';
+        this.loading = false;
+        this.getCategories()
+      },
+      error: (error) => {
+        this.loading = false;
+        this.errorMessage = error.error.message || 'An error occurred while adding the category.';       
+      }
+    });
+  }  
+  private getCategories() {
+    this.loading = true;
+    this.adminService.getCategories(this.userId).pipe(takeUntil(this.destroy$)).subscribe({
+      next: (categories: Category[]) => {
+        this.categories = categories;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage = err.error.message || 'An error occurred while fetching categories.';
+      }
+    });
+  }
+
+  public deleteCategory(categoryId: number) {
+    this.loading = true;
+    this.adminService.deleteCategory(categoryId).pipe(takeUntil(this.destroy$)).subscribe({
+      next: () => {
+        this.getCategories();
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+  public back() {
+    window.history.back();
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }

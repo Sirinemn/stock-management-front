4bb16a9a8ac53019a4054bf744d81eb11cdc343f
Sirinemn <sirinemn@outlook.fr>
2025-04-29 12:00:10 +0200

    fix product-form test

--- src/app/features/inventory/products/components/product-form/product-form.component.spec.ts
+++ src/app/features/inventory/products/components/product-form/product-form.component.spec.ts
@@ -3,17 +3,37 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductFormComponent } from './product-form.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { SessionService } from '../../../../../core/services/session.service';
 
 describe('ProductFormComponent', () => {
   let component: ProductFormComponent;
   let fixture: ComponentFixture<ProductFormComponent>;
+  let mockSessionService: Partial<SessionService>;
+  let mockUser = {
+    id: 1,
+    firstname: 'first',
+    lastname: 'last',
+    email: 'email@test.fr',
+    dateOfBirth: '01/02/1992',
+    createdDate: '10/04/2025',
+    lastModifiedDate: '10/04/2025',
+    firstLogin: false,
+    roles: ['USER'],
+    groupId: 1,
+    groupName: 'group',
+    createdBy: 2,
+  }
 
   beforeEach(async () => {
+    mockSessionService = {getUser: jest.fn().mockReturnValue(mockUser)}
+    
     await TestBed.configureTestingModule({
       imports: [ProductFormComponent],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        { provide: SessionService, useValue: mockSessionService },
+
       ]
     })
     .compileComponents();

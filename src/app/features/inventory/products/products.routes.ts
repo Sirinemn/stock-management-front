2236760c11f1a2ce63c635d088e3631fd46864d8
Sirinemn <sirinemn@outlook.fr>
2025-05-01 17:01:05 +0200

    add view route

--- src/app/features/inventory/products/products.routes.ts
+++ src/app/features/inventory/products/products.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { ProductFormComponent } from "./components/product-form/product-form.component";
+import { ProductDetailComponent } from "./components/product-detail/product-detail.component";
 
 export const products_routes: Routes = [
     {title: 'Products', path: 'list', component: ProductListComponent },
     {title: 'Products', path: 'edit/:id', component: ProductFormComponent },
     {title: 'Products', path: 'add', component: ProductFormComponent },
+    {title: 'Products', path: 'view', component: ProductDetailComponent}
 ]

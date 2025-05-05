0ebe9795e5c61c80dd46c2095ffa2e09896b7644
Sirinemn <sirinemn@outlook.fr>
2025-05-05 17:14:50 +0200

    add stock route in features routes

--- src/app/features/inventory/features.routes..ts
+++ src/app/features/inventory/features.routes..ts
@@ -4,4 +4,5 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 export const features_routes: Routes = [
   { title: 'Dashboard', path: 'dashboard', component: DashboardComponent },
   { path: 'products', loadChildren: () => import('./products/products.routes').then((route) => route.products_routes) },
+  { path: 'stock', loadChildren: () => import('./stock-movement/stock-movement.routes').then((route) => route.stock_routes)}
 ];

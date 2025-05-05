5f60e64f66d3d27ae41bcfd319ab4361c1ef79e1
Sirinemn <sirinemn@outlook.fr>
2025-05-05 11:28:32 +0200

    add stock-movement-form component

---
+++ src/app/features/inventory/stock-movement/componnet/stock-movement-form/stock-movement-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-movement-form',
+  imports: [],
+  templateUrl: './stock-movement-form.component.html',
+  styleUrl: './stock-movement-form.component.scss'
+})
+export class StockMovementFormComponent {
+
+}

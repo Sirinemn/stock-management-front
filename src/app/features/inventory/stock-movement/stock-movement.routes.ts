0bb08341fd8b6d38cf2f4e9c4df850db8920f8ff
Sirinemn <sirinemn@outlook.fr>
2025-05-05 17:12:32 +0200

    implement stock-movement routes

---
+++ src/app/features/inventory/stock-movement/stock-movement.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { StockMovementListComponent } from "./componnet/stock-movement-list/stock-movement-list.component";
+import { StockMovementFormComponent } from "./componnet/stock-movement-form/stock-movement-form.component";
+
+export const stock_routes: Routes = [
+    {title: 'Stock', path: 'list', component: StockMovementListComponent },
+    {title: 'Stock', path: 'edit/:id', component: StockMovementFormComponent },
+    {title: 'Stock', path: 'add', component: StockMovementFormComponent },
+    //{title: 'Stock', path: 'view/:id', component: ProductDetailComponent}
+]

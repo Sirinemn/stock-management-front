eb46b3577f8496a4e8a848b972e9326a841635b5
Sirinemn <sirinemn@outlook.fr>
2025-05-06 19:22:29 +0200

    add userName to StockMovement interface

--- src/app/features/inventory/models/stockmovement.ts
+++ src/app/features/inventory/models/stockmovement.ts
@@ -2,6 +2,8 @@ export interface StockMovement {
     id?: number;
     productId: number;
     userId: number;
+    userName: string;
+    productName: string;
     type: 'ENTREE' | 'SORTIE';
     quantity: number;
     date?: string;

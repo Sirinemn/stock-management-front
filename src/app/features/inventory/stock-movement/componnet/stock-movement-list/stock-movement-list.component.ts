a6adafeb153b56ea7bd5d87927d955126b2d8fd9
Sirinemn <sirinemn@outlook.fr>
2025-05-07 20:51:22 +0200

    implement stock-movement-list

--- src/app/features/inventory/stock-movement/componnet/stock-movement-list/stock-movement-list.component.ts
+++ src/app/features/inventory/stock-movement/componnet/stock-movement-list/stock-movement-list.component.ts
@@ -7,8 +7,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { StockMovement } from '../../../models/stockmovement';
+import { SessionService } from '../../../../../core/services/session.service';
+import { StockMovementService } from '../../service/stock-movement.service';
+import { AuthStateService } from '../../../../../core/services/auth-state.service';
+import { User } from '../../../../../auth/models/user';
 
 @Component({
   selector: 'app-stock-movement-list',
@@ -22,13 +26,22 @@ export class StockMovementListComponent implements OnInit, OnDestroy {
   public errorMessage: string = '';
   public movements: StockMovement[] = [];
   public isAdmin: boolean = false;
-  public displayedColumns: string[] = ['product', 'quantity', 'createdBy', 'createdAt', 'actions'];
+  private user!: User;
+  public displayedColumns: string[] = ['productName', 'quantity', 'createdBy', 'createdAt', 'actions'];
   public isLoading = false;
   public groupId: number = 0;
   public filters = {userId: null, productId: null, groupId: null, startDate: null, endDate: null}
 
+  constructor(
+    private sessionService: SessionService,
+    private authStateService: AuthStateService,
+    private stockMovementService: StockMovementService,
+  ) {}
+
   ngOnInit(): void {
-      
+    this.loadDisplayedcolumns();
+    this.getUser();
+    this.loadStockMovements();
   }
 
   public deleteProduct(_t77: any) {
@@ -37,6 +50,37 @@ export class StockMovementListComponent implements OnInit, OnDestroy {
   }
   public viewProduct(arg0: any) {
   }
+  public loadDisplayedcolumns() {
+    this.authStateService.getIsAdmin().subscribe((isAdmin) => {
+      this.isAdmin = isAdmin;
+      if (isAdmin) {
+        this.displayedColumns = ['productName', 'quantity', 'createdBy', 'createdAt', 'actions'];
+      } else {
+        this.displayedColumns = ['productName', 'quantity', 'createdAt'];
+      }
+    });
+  }
+  public getUser() {
+    this.sessionService.getUser$().pipe(takeUntil(this.destroy$)).subscribe((user) => {
+      this.user = user!;
+      this.groupId = user!.groupId;
+      
+    });
+  }
+  public loadStockMovements() {
+    this.isLoading = true;
+    this.stockMovementService.getStockMovementsByGroup(this.groupId).subscribe({
+      next: (response) => {
+        this.movements = response;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+        this.isLoading = false;
+      }
+    });
+  }
+
 
   public back() {
     window.history.back();

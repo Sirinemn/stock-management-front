c0f43dd6f5e6870bce4b4dcb482883aa47290f74
Sirinemn <sirinemn@outlook.fr>
2025-05-12 11:09:38 +0200

    add stock-movement-detail component

---
+++ src/app/features/inventory/stock-movement/componnet/stock-movement-detail/stock-movement-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-movement-detail',
+  imports: [],
+  templateUrl: './stock-movement-detail.component.html',
+  styleUrl: './stock-movement-detail.component.scss'
+})
+export class StockMovementDetailComponent {
+
+}

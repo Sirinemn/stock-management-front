995d17ba91f65244a9abddbb84862c974117a377
Sirinemn <sirinemn@outlook.fr>
2025-05-12 19:11:53 +0200

    add stock-movement-filter route

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from "@angular/router";
 import { UserFormComponent } from "./users/user-form/user-form.component";
 import { UsersListComponent } from "./users/users-list/users-list.component";
 import { UserDetailComponent } from "./users/user-detail/user-detail.component";
+import { StockMovementFilterComponent } from "./stock-movement-filter/stock-movement-filter.component";
 
 export const admin : Routes = [
     {title: 'Users', path: 'users', component: UsersListComponent},
     {title: 'AddUser', path: 'adduser', component: UserFormComponent},
     {title: 'UserDetail', path: 'user/view/:id', component: UserDetailComponent},
     {title: 'EditUser', path: 'user/edit/:id', component: UserFormComponent},
+    {title:'StockFilter', path: 'stockfilter', component: StockMovementFilterComponent},
     {title: 'Categories', path: 'categories', loadChildren: () => import('./categories/category.routes').then(m => m.categoryRoutes)},
     
 ]

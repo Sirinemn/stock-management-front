72b575a315c5ace8c0d9d78bf94a8b080355951a
Sirinemn <sirinemn@outlook.fr>
2025-05-13 17:57:23 +0200

    implement password-change-dialog tests

--- src/app/mat-dialog/password-change-dialog/password-change-dialog.component.spec.ts
+++ src/app/mat-dialog/password-change-dialog/password-change-dialog.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordChangeDialogComponent } from './password-change-dialog.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('PasswordChangeDialogComponent', () => {
   let component: PasswordChangeDialogComponent;
   let fixture: ComponentFixture<PasswordChangeDialogComponent>;
+  const mockDialogRef = {
+    close: jest.fn()
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordChangeDialogComponent]
+      imports: [PasswordChangeDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +26,12 @@ describe('PasswordChangeDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should close dialog with redirect action', () => {
+    const closeSpy = jest.spyOn(mockDialogRef, 'close');
+
+    component.redirectToChangePassword();
+
+    expect(closeSpy).toHaveBeenCalledWith('redirect');
   });
 });

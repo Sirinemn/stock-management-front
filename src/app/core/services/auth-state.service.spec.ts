656ca0493144db3e4603beeebb82fc7d4a3667e0
Sirinemn <sirinemn@outlook.fr>
2025-05-13 17:14:00 +0200

    implement au-state service

--- src/app/core/services/auth-state.service.spec.ts
+++ src/app/core/services/auth-state.service.spec.ts
@@ -13,4 +13,32 @@ describe('AuthStateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should initialize with default values', () => {
+    expect(service.userRole).toEqual([]);
+    service.getFirstLogin().subscribe(value => expect(value).toBeFalsy());
+    service.getIsAdmin().subscribe(value => expect(value).toBeFalsy());
+  });
+  it('should update and retrieve first login status', () => {
+    service.setFirstLogin(true);
+    service.getFirstLogin().subscribe(value => expect(value).toBeTruthy());
+
+    service.setFirstLogin(false);
+    service.getFirstLogin().subscribe(value => expect(value).toBeFalsy());
+  });
+  it('should update and retrieve admin status', () => {
+    service.setIsAdmin(true);
+    service.getIsAdmin().subscribe(value => expect(value).toBeTruthy());
+
+    service.setIsAdmin(false);
+    service.getIsAdmin().subscribe(value => expect(value).toBeFalsy());
+  });
+  it('should return true if userRole contains ADMIN', () => {
+    service.userRole = ['USER', 'ADMIN'];
+    expect(service.isAdmin()).toBeTruthy();
+  });
+
+  it('should return false if userRole does not contain ADMIN', () => {
+    service.userRole = ['USER'];
+    expect(service.isAdmin()).toBeFalsy();
+  });
 });

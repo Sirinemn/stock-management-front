066bcbf5dcfc3b06270f979eed0d94e37b20132f
Sirinemn <sirinemn@outlook.fr>
2025-05-15 10:25:12 +0200

    reorganise packages

--- src/app/features/inventory/features.routes..ts
+++ src/app/features/inventory/features.routes..ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './charts/dashboard/dashboard.component';
 
 export const features_routes: Routes = [
   { title: 'Dashboard', path: 'dashboard', component: DashboardComponent },

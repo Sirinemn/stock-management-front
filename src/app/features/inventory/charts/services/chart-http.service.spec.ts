9ecd90510ce28827f087118d26d2040bc2fde276
Sirinemn <sirinemn@outlook.fr>
2025-05-19 22:32:55 +0200

    add tests to chat-http service

--- src/app/features/inventory/charts/services/chart-http.service.spec.ts
+++ src/app/features/inventory/charts/services/chart-http.service.spec.ts
@@ -2,10 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { ChartHttpService } from './chart-http.service';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { DashboardOverview } from '../models/dashboardOverview.model';
+import { ProductQuantity } from '../models/product-quantity.model';
+import { StockChartSeries } from '../models/stock-chart-series.model';
 
 describe('ChartHttpService', () => {
   let service: ChartHttpService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +19,48 @@ describe('ChartHttpService', () => {
       ]
     });
     service = TestBed.inject(ChartHttpService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should fetch dashboard overview', () => {
+    const mockOverview = { 
+      totalProducts: 100, lowStockProducts: 20 
+    } as DashboardOverview;
+
+    service.getDashboardOverview(1).subscribe(overview => {
+      expect(overview).toEqual(mockOverview);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/overview?groupId=1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockOverview);
+  });
+  it('should fetch pie chart data', () => {
+    const mockData: ProductQuantity[] = [{ productName: 'Product A', quantity: 50 }];
+
+    service.getPieChartData(1).subscribe(data => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/pie?groupId=1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData);
+  });
+  it('should fetch line chart data', () => {
+    const mockData: StockChartSeries[] = [{ name: 'Stock Flow', series: [{ date: '2023-01-01', value: 100 }] }];
+
+    service.getLineChartData(1).subscribe(data => {
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}/line?groupId=1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockData);
   });
 });

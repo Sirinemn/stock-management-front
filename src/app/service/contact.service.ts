4ab5a0de094da6dce8894406c24371e150d5cc10
Sirinemn <sirinemn@outlook.fr>
2025-05-30 13:13:05 +0200

    implement contact-service

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MessageResponse } from '../shared/models/messageResponse';
+import { ContactRequest } from '../shared/models/contactRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private apiUrl = 'http://localhost:8080/api/contact';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public sendEmail(contactRequest: ContactRequest): Observable<MessageResponse> {
+    return this.http.post<MessageResponse>(this.apiUrl, contactRequest)
+  }
 }

926bcf5b8eaf23269565c51164ca827e0c2a3f9a
Sirinemn <sirinemn@outlook.fr>
2025-05-31 18:40:50 +0200

    import RouterLink in terms component

--- src/app/pages/terms/terms.component.spec.ts
+++ src/app/pages/terms/terms.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TermsComponent } from './terms.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TermsComponent', () => {
   let component: TermsComponent;
   let fixture: ComponentFixture<TermsComponent>;
+    let activatedRouteMock: any= {
+          snapshot: {
+            paramMap: of({})
+          }
+        };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TermsComponent]
+      imports: [TermsComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+      ]
     })
     .compileComponents();
 

45bc7192f46880e449b0dfb0b20eca3ea37db938
Sirinemn <sirinemn@outlook.fr>
2025-05-31 18:40:32 +0200

    import RouterLink in privacy component

--- src/app/pages/privacy/privacy.component.spec.ts
+++ src/app/pages/privacy/privacy.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrivacyComponent } from './privacy.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('PrivacyComponent', () => {
   let component: PrivacyComponent;
   let fixture: ComponentFixture<PrivacyComponent>;
+  let activatedRouteMock: any= {
+        snapshot: {
+          paramMap: of({})
+        }
+      };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PrivacyComponent]
+      imports: [PrivacyComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+      ]
     })
     .compileComponents();
 
--- src/app/pages/privacy/privacy.component.ts
+++ src/app/pages/privacy/privacy.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
